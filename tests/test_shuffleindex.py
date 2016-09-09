f0152d5281653326f1dedfe4566b91b268421beb
enricobacis <enrico.bacis@gmail.com>
2016-09-09 14:08:11 +0200

    use StatsLayer in test (now correctly failing)

--- tests/test_shuffleindex.py
+++ tests/test_shuffleindex.py
@@ -1,12 +1,13 @@
+from shuffleindex.datalayer.statslayer import *
 from shuffleindex.datalayer.datalayer import *
 from shuffleindex.shuffleindex import *
 import pytest
 
-class TestBPTree:
+class TestShuffleTree:
 
     def setup_method(self, test_method):
-        datalayer = MemoryDataLayer()
-        self.tree = Tree(datalayer, fanout=2, leafsize=2)
+        self.datalayer = StatsLayer(MemoryDataLayer())
+        self.tree = Tree(self.datalayer, fanout=2, leafsize=2)
         self.tree.bulk_load({1:'a', 2:'b', 3:'c', 4:'d', 5:'e'})
 
     def test_search_with_included_key(self):
@@ -15,3 +16,10 @@ class TestBPTree:
     def test_search_with_not_included_key(self):
         with pytest.raises(KeyError):
             self.tree.search(6)
+
+    def test_statistics(self):
+        n = 10                                              # number of accesses
+        for _ in xrange(n): self.tree.search(1)           # access key 1 n times
+        mc1, mc2 = self.datalayer.getcount.most_common(2)    # get most accessed
+        assert mc1[1] == n                               # root accessed n times
+        assert mc2[1] != n              # other nodes accessed less then n times

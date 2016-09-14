9dbf91d1431e91934ca30d715726674f39fb0139
enricobacis <enrico.bacis@gmail.com>
2016-09-14 11:48:08 +0200

    disable statistics shuffle index test since this is gonna change

--- tests/test_shuffleindex.py
+++ tests/test_shuffleindex.py
@@ -1,6 +1,7 @@
 from shuffleindex.datalayer.memorylayer import *
 from shuffleindex.datalayer.statslayer import *
 from shuffleindex.shuffleindex import *
+from string import printable
 import pytest
 
 class TestShuffleTree:
@@ -8,18 +9,18 @@ class TestShuffleTree:
     def setup_method(self, test_method):
         self.datalayer = StatsLayer(MemoryDataLayer())
         self.tree = Tree(self.datalayer, fanout=2, leafsize=2)
-        self.tree.bulk_load({1:'a', 2:'b', 3:'c', 4:'d', 5:'e'})
+        self.tree.bulk_load(dict(enumerate(printable)))
 
     def test_search_with_included_key(self):
-        assert self.tree.search(5) == 'e'
+        assert self.tree.search(0) == printable[0]
 
     def test_search_with_not_included_key(self):
         with pytest.raises(KeyError):
-            self.tree.search(6)
+            self.tree.search(len(printable))
 
-    def test_statistics(self):
-        n = 10                                              # number of accesses
-        for _ in xrange(n): self.tree.search(1)           # access key 1 n times
-        mc1, mc2 = self.datalayer.getcount.most_common(2)    # get most accessed
-        assert mc1[1] == n                               # root accessed n times
-        assert mc2[1] != n              # other nodes accessed less then n times
+#    def test_statistics(self):
+#        n = 10                                              # number of accesses
+#        for _ in xrange(n): self.tree.search(1)           # access key 1 n times
+#        mc1, mc2 = self.datalayer.getcount.most_common(2)    # get most accessed
+#        assert mc1[1] == n                               # root accessed n times
+#        assert mc2[1] != n              # other nodes accessed less then n times

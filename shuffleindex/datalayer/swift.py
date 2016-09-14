3696c686199b9be8ed8f23e2139a240b68cb21b9
Marco Rosa <marcor165@hotmail.it>
2016-09-14 12:24:04 +0200

    Add swift interface implementation

--- shuffleindex/datalayer/swift.py
+++ shuffleindex/datalayer/swift.py
@@ -1,12 +1,20 @@
 from datalayer import DataLayer
+from swiftclient import client
+
 
 class SwiftDataLayer(DataLayer):
 
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, authurl, username, key, tenant_name,
+                 container_name, auth_version='2.0'):
+        self.swift = client.Connection(authurl=authurl,
+                                       user=username,
+                                       key=key,
+                                       tenant_name=tenant_name,
+                                       auth_version=auth_version)
+        self.container = container_name
 
     def get(self, key):
-        raise NotImplementedError
+        return self.swift.get_object(container=self.container, obj=key)
 
     def put(self, key, value):
-        raise NotImplementedError
+        self.swift.put_object(container=self.container, obj=key, contents=value)

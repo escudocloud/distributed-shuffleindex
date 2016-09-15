9243ff777f8b7635994b0c2d3f0cd9255450eade
enricobacis <enrico.bacis@gmail.com>
2016-09-15 10:23:39 +0200

    add plot methods (need asserts) to test suite

--- tests/test_shuffleindex.py
+++ tests/test_shuffleindex.py
@@ -1,15 +1,21 @@
+from shuffleindex.datalayer.shufflelayer import *
 from shuffleindex.datalayer.memorylayer import *
 from shuffleindex.datalayer.statslayer import *
 from shuffleindex.shuffleindex import *
+from testutils import gaussrange
 from string import printable
 import pytest
+
+N = 1000                    # number of accesses to the datalayer in below tests
 
 class TestShuffleTree:
 
     def setup_method(self, test_method):
-        self.datalayer = StatsLayer(MemoryDataLayer())
-        self.tree = Tree(self.datalayer, fanout=2, leafsize=2)
-        self.tree.bulk_load(dict(enumerate(printable)))
+        Node.set_last_id(0)                            # reset node counter to 0
+        self.datalayer = StatsLayer(MemoryDataLayer()) # stats-memory-data-layer
+        self.data = dict(enumerate(printable))                     # create data
+        self.tree = Tree(self.datalayer, fanout=10, leafsize=2)    # create tree
+        self.tree.bulk_load(self.data)            # bulk load data into the tree
 
     def test_search_with_included_key(self):
         assert self.tree.search(0) == printable[0]
@@ -18,9 +24,16 @@ class TestShuffleTree:
         with pytest.raises(KeyError):
             self.tree.search(len(printable))
 
-#    def test_statistics(self):
-#        n = 10                                              # number of accesses
-#        for _ in xrange(n): self.tree.search(1)           # access key 1 n times
-#        mc1, mc2 = self.datalayer.getcount.most_common(2)    # get most accessed
-#        assert mc1[1] == n                               # root accessed n times
-#        assert mc2[1] != n              # other nodes accessed less then n times
+    def test_without_shuffle_index(self):
+        for i in xrange(N):
+            self.tree.search(gaussrange(len(self.data)))
+        self.datalayer.plot_get()
+        self.datalayer.plot_put()
+
+    def test_with_shuffle_index(self):
+        shufflelayer = ShuffleLayer(self.datalayer, num_cover=2, num_cache=1)
+        shufflelayer.init_cache(self.tree._root)
+        for i in xrange(N):
+            shufflelayer.get(gaussrange(len(self.data)))
+        self.datalayer.plot_get()
+        self.datalayer.plot_put()

f1f61b4442667dfd64bc733fc5fb5411e84c0858
Marco Rosa <marcor165@hotmail.it>
2016-09-15 12:19:01 +0200

    Fix test for swift

--- tests/test_swift.py
+++ tests/test_swift.py
@@ -8,25 +8,25 @@ data = [(1, 'a'), (2, 'b'), (3, 'c'), (4, 'd'), (5, 'd')]
 
 class TestSwift:
 
-    def setup(self):
+    def setup_method(self, test_method):
         self.swift = SwiftDataLayer(authurl, user, password,
                                     tenant_name, container_name)
 
-    def test_put(self):
+    def _put(self):
         for i in range(len(data)):
             self.swift.put(data[i][0], data[i][1])
 
     def test_get_existing_keys(self):
+        self._put()
         for i in range(len(data)):
-            self.swift.get(data[i][0])
+            assert self.swift.get(data[i][0])[1] == data[i][1]
 
     def test_get_missing_keys(self):
+        self._put()
         keys = [10, 15]
         for k in keys:
             with pytest.raises(ClientException):
                 self.swift.get(k)
 
-    def clean(self):
-        obj_head, objs = self.swift.swift.get_container(container_name)
-        for o in objs:
-            self.swift.swift.delete_object(container=container_name, obj=o['name'])
+    def teardown_method(self, test_method):
+        self.swift.clean()

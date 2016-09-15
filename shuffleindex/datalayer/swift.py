b72ac2cc4593fb534b9a533c76a934a662cb4536
Marco Rosa <marcor165@hotmail.it>
2016-09-15 12:18:31 +0200

    Add clean method and set variables visibility

--- shuffleindex/datalayer/swift.py
+++ shuffleindex/datalayer/swift.py
@@ -6,15 +6,20 @@ class SwiftDataLayer(DataLayer):
 
     def __init__(self, authurl, username, key, tenant_name,
                  container_name, auth_version='2.0'):
-        self.swift = client.Connection(authurl=authurl,
-                                       user=username,
-                                       key=key,
-                                       tenant_name=tenant_name,
-                                       auth_version=auth_version)
-        self.container = container_name
+        self._swift = client.Connection(authurl=authurl,
+                                        user=username,
+                                        key=key,
+                                        tenant_name=tenant_name,
+                                        auth_version=auth_version)
+        self._container = container_name
 
     def get(self, key):
-        return self.swift.get_object(container=self.container, obj=key)
+        return self._swift.get_object(container=self._container, obj=key)
 
     def put(self, key, value):
-        self.swift.put_object(container=self.container, obj=key, contents=value)
+        self._swift.put_object(container=self._container, obj=key, contents=value)
+
+    def clean(self):
+        _, objs = self._swift.get_container(self._container)
+        for o in objs:
+            self._swift.delete_object(container=self._container, obj=o['name'])

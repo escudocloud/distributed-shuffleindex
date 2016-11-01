f3fb05635053c479d38bf1b1306b7e05723e0d38
Marco Rosa <marcor165@hotmail.it>
2016-11-01 12:05:55 +0100

    fix gcs test

--- tests/test_gcs.py
+++ tests/test_gcs.py
@@ -7,13 +7,13 @@ except ImportError:
     pytestmark = pytest.mark.skipif(True, reason='missing configuration file')
 
 
-data = [(1, 'a'), (2, 'b'), (3, 'c'), (4, 'd'), (5, 'd')]
+data = [('1', 'a'), ('2', 'b'), ('3', 'c'), ('4', 'd'), ('5', 'd')]
 
 
 class TestGCS:
 
     def setup_method(self, test_method):
-        self.gcs = GCSDataLayer('configs.gcs_key.json', project, bucket)
+        self.gcs = GCSDataLayer('configs/gcs_key.json', project, bucket)
 
     def _put(self):
         for i in range(len(data)):
@@ -22,13 +22,13 @@ class TestGCS:
     def test_get_existing_keys(self):
         self._put()
         for i in range(len(data)):
-            assert self.gcs.get(data[i][0])[1] == data[i][1]
+            assert self.gcs.get(data[i][0]) == data[i][1]
 
     def test_get_missing_keys(self):
         self._put()
         keys = [10, 15]
         for k in keys:
-            with pytest.raises(HttpError):
+            with pytest.raises(Exception):
                 self.gcs.get(k)
 
     def teardown_method(self, test_method):

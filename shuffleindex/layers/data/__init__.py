f4f6ee5419c8f7f3a75254a592ed7dc984d2f0fa
enricobacis <enrico.bacis@gmail.com>
2016-11-22 19:11:25 +0100

    make datalayers import optional

--- shuffleindex/layers/data/__init__.py
+++ shuffleindex/layers/data/__init__.py
@@ -1,4 +1,16 @@
 from memorylayer import MemoryDataLayer
-from swift import SwiftDataLayer
-from gcs import GCSDataLayer
-from s3 import S3DataLayer
+
+try:
+    from swift import SwiftDataLayer
+except ImportError:
+    print 'warning: cannot import swift library'
+
+try:
+    from gcs import GCSDataLayer
+except ImportError:
+    print 'warning: cannot import gcs library'
+
+try:
+    from s3 import S3DataLayer
+except ImportError:
+    print 'warning: cannot import s3 library'
